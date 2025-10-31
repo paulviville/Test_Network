1f21e36d210adf68ee7509e7def392242396a2e7
paulviville <paul.viville@gmail.com>
2025-10-31 15:15:21 +0100

    in instance messages

--- Commands.js
+++ Commands.js
@@ -1,11 +1,25 @@
 export default class Commands {
 	static SERVER_ID = 0XFFFFFFFF;
 
+	/// Sent by the SERVER when encountering a bad user input
+	static ERROR = "ERROR";
+
+	/// Sent by the SERVER to give an ID to the new user on connection
     static SET_USER = "SET_USER";
 
+	/// Sent by the SERVER to provide the instances list to users on connection and on update
 	static INSTANCE_LIST = "INSTANCE_LIST";
+	/// Sent by the USERS to request creation of new instances
 	static INSTANCE_NEW = "INSTANCE_NEW";
+	/// Sent by the USERS to request the deletion of instances
 	static INSTANCE_DELETE = "INSTANCE_DELETE";
+	/// Sent by the USERS to join an instance
 	static INSTANCE_JOIN = "INSTANCE_JOIN";
+	/// Sent by the USERS to leave an instance
 	static INSTANCE_LEAVE = "INSTANCE_LEAVE";
+
+	/// Sent by the SERVER to instance users to announce USER joining the instance
+	static NEW_USER = "NEW_USER";
+	/// Sent by the SERVER to instance users to announce USER leaving the instance
+    static REMOVE_USER = "REMOVE_USER";
 }
--- Messages.js
+++ Messages.js
@@ -11,6 +11,10 @@ export default class Messages {
 		return JSON.stringify( message );
 	}
 
+	static error ( errorData ) {
+		return this.create( Commands.ERROR, Commands.SERVER_ID, { errorData } );
+	}
+
 	static setUser ( userId ) {
 		return this.create( Commands.SET_USER, Commands.SERVER_ID, { userId } );
 	}
@@ -19,19 +23,27 @@ export default class Messages {
 		return this.create( Commands.INSTANCE_LIST, Commands.SERVER_ID, { instancesList } );
 	}
 
-	static newInstance ( userId, instanceName ) {
-		return this.create( Commands.INSTANCE_NEW, userId, { instanceName } );
+	static newInstance ( senderId, instanceName ) {
+		return this.create( Commands.INSTANCE_NEW, senderId, { instanceName } );
 	}
 
-	static deleteInstance ( userId, instanceName ) {
-		return this.create( Commands.INSTANCE_DELETE, userId, { instanceName } );
+	static deleteInstance ( senderId, instanceName ) {
+		return this.create( Commands.INSTANCE_DELETE, senderId, { instanceName } );
 	}
 
-	static joinInstance ( userId, instanceName ) {
-		return this.create( Commands.INSTANCE_JOIN, userId, { instanceName } );
+	static joinInstance ( senderId, instanceName ) {
+		return this.create( Commands.INSTANCE_JOIN, senderId, { instanceName } );
 	}
 
-	static leaveInstance ( userId, instanceName ) {
-		return this.create( Commands.INSTANCE_LEAVE, userId, { instanceName } );
+	static leaveInstance ( senderId, instanceName ) {
+		return this.create( Commands.INSTANCE_LEAVE, senderId, { instanceName } );
+	}
+
+	static newUser ( senderId, userId ) {
+		return this.create( Commands.NEW_USER, senderId, { userId } );
+	}
+
+	static removeUser ( senderId, userId ) {
+		return this.create( Commands.REMOVE_USER, senderId, { userId } );
 	}
 }
