199470d782071da7d51a0b636bd23921c3ecf9ad
paulviville <paul.viville@gmail.com>
2025-10-31 16:50:28 +0100

    rewrote all commands and messages

--- Commands.js
+++ Commands.js
@@ -18,8 +18,63 @@ export default class Commands {
 	/// Sent by the USERS to leave an instance
 	static INSTANCE_LEAVE = "INSTANCE_LEAVE";
 
+
+
+
 	/// Sent by the SERVER to instance users to announce USER joining the instance
 	static NEW_USER = "NEW_USER";
 	/// Sent by the SERVER to instance users to announce USER leaving the instance
     static REMOVE_USER = "REMOVE_USER";
+
+	/// Sent by USER to SERVER to update camera position, broadcast to instance users
+	static UPDATE_CAMERA = "UPDATE_CAMERA";
+    
+	/// Sent by USER to SERVER to signal pointer start, broadcast to instance users
+    static START_POINTER = "START_POINTER";
+	/// Sent by USER to SERVER to signal pointer end, broadcast to instance users
+    static END_POINTER = "END_POINTER";
+	/// Sent by USER to SERVER to update pointer data, broadcast to instance users
+    static UPDATE_POINTER = "UPDATE_POINTER";
+
+	/// Sent by the USERS to request node selection, broadcast to instance users if approved
+    static SELECT = "SELECT";
+	/// Sent by the USERS to request node deselection, broadcast to instance users if approved
+    static DESELECT = "DESELECT";
+	/// Sent by USER to SERVER to signal node transform modification start
+    static START_TRANSFORM = "START_TRANSFORM";
+	/// Sent by USER to SERVER to signal node transform modification end
+    static END_TRANSFORM = "END_TRANSFORM";
+	/// Sent by USER to SERVER to update node transform, broadcast to instance users
+    static UPDATE_TRANSFORM = "UPDATE_TRANSFORM";
+
+	/// Sent by the USERS to request new primitive, broadcast to instance users
+	static ADD_PRIMITIVE = "ADD_PRIMITIVE";
+	static Primitives = {
+		Sphere: "Sphere",
+		Cylinder: "Cylinder",
+		Cube: "Cube",
+		Quad: "Quad",
+		Capsule: "Capsule",
+	}
+	/// Sent by the USERS to request node deletion, broadcast to instance users if approved
+	static DELETE_NODE = "DELETE_NODES";
+
+	/// Sent by the USERS to add marker, broadcast to instance users if approved
+    static ADD_MARKER = "ADD_MARKER";
+	/// Sent by the USERS to delete marker, broadcast to instance users if approved
+    static DELETE_MARKER = "DELETE_MARKER";
+	/// Sent by the USERS to update marker data, broadcast to instance users if approved
+    static UPDATE_MARKER = "UPDATE_MARKER";
+
+	/// Sent by the USERS to add annotation, broadcast to instance users if approved
+	static ADD_ANNOTATION = "ADD_ANNOTATION";
+	/// Sent by the USERS to delete annotation, broadcast to instance users if approved
+	static DELETE_ANNOTATION = "DELETE_ANNOTATION";
+	/// Sent by the USERS to update annotation, broadcast to instance users if approved
+	static UPDATE_ANNOTATION = "UPDATE_ANNOTATION";
+
+	/// Sent by the USERS to synchronize custom signal, broadcast to instance users
+	static LAMBDA = "LAMBDA";
+
+
 }
--- Messages.js
+++ Messages.js
@@ -39,11 +39,100 @@ export default class Messages {
 		return this.create( Commands.INSTANCE_LEAVE, senderId, { instanceName } );
 	}
 
-	static newUser ( senderId, userId ) {
-		return this.create( Commands.NEW_USER, senderId, { userId } );
+	static newUser ( userId ) {
+		return this.create( Commands.NEW_USER, Commands.SERVER_ID, { userId } );
 	}
 
-	static removeUser ( senderId, userId ) {
-		return this.create( Commands.REMOVE_USER, senderId, { userId } );
+	static removeUser ( userId ) {
+		return this.create( Commands.REMOVE_USER, Commands.SERVER_ID, { userId } );
+	}
+
+	/// cameraMatrix: mat44
+	static updateCamera ( senderId, cameraMatrix ) {
+		return this.create( Commands.UPDATE_CAMERA, senderId, { cameraMatrix } );
+	}
+
+	static startPointer ( senderId ) {
+		return this.create( Commands.START_POINTER, senderId );
+	}
+
+	static endPointer ( senderId ) {
+		return this.create( Commands.END_POINTER, senderId );
+	}
+
+	/// pointer: { origin: vec3, end: vec3 } 
+	static updatePointer ( senderId, pointer ) {
+		return this.create( Commands.END_POINTER, senderId, { pointer } );
+	}
+
+	/// node: { id: uint }
+	static select ( senderId, nodes ) {
+		return this.create( Commands.SELECT, senderId, { nodes } );
+	}
+
+	/// node: { id: uint }
+	static deselect ( senderId, nodes ) {
+		return this.create( Commands.DESELECT, senderId, { nodes } );
+	}
+
+	/// node: { id: uint }
+	static deleteNode ( senderId, nodes ) {
+		return this.create( Commands.DELETE_NODE, senderId, { nodes } );
+		
+	}
+
+	/// primitive: { tpye: string }
+	static addPrimitive ( senderId, primitive ) {
+		return this.create( Commands.ADD_PRIMITIVE, senderId, { primitive } );
+	}
+
+	/// node: { id: uint, matrix: mat44 }
+	static startTransform ( senderId, nodes ) {
+		return this.create( Commands.START_TRANSFORM, senderId, { nodes } );
+	}
+
+	/// node: { id: uint }
+	static endTransform ( senderId, nodes ) {
+		return this.create( Commands.END_TRANSFORM, senderId, { nodes } );
+	}
+
+	/// node: { id: uint, matrix: mat44 }
+	static updateTransform ( senderId, nodes ) {
+		return this.create( Commands.UPDATE_TRANSFORM, senderId, { nodes } );
+	}
+
+	/// marker: { origin: vec3, end: vec3 }
+	static addMarker ( senderId, marker ) {
+		return this.create( Commands.ADD_MARKER, senderId, { marker } );
+	}
+
+	/// marker: { id: uint }
+	static deleteMarker ( senderId, marker ) {
+		return this.create( Commands.DELETE_MARKER, senderId, { marker } );
+	}
+
+	/// marker: { id: uint, origin: vec3, end: vec3 }
+	static updateMarker ( senderId, marker ) {
+		return this.create( Commands.UPDATE_MARKER, senderId, { marker } );
+	}
+
+	/// annotation: { position: vec3, text: string }
+	static addAnnotation ( senderId, annotation ) {
+		return this.create( Commands.ADD_ANNOTATION, senderId, { annotation } );
+	}
+
+	/// annotation: { id: uint }
+	static deleteAnnotation ( senderId, annotation ) {
+		return this.create( Commands.DELETE_ANNOTATION, senderId, { annotation } );
+	}
+
+	/// annotation: { id: uint, position: vec3, text: string }
+	static updateAnnotation ( senderId, annotation ) {
+		return this.create( Commands.UPDATE_ANNOTATION, senderId, { annotation } );
+	}
+
+	/// lambdaData: { label: string, data: {...}}
+	static lambda ( senderId, lambdaData ) {
+		return this.create( Commands.LAMBDA, senderId, { lambdaData } );
 	}
 }
